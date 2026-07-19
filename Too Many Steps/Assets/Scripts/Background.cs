b9d33d00d371b2fac0301d0f3836af7f9c0926d6
Sanj1t <rannenchi009sharingantoad2@gmail.com>
2026-07-19 17:22:26 +0530

    Added animations

---
+++ Too Many Steps/Assets/Scripts/Background.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private float activeTime;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 moveDirection;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        rb.linearVelocity = Vector2.MoveTowards(transform.position, moveDirection, Mathf.Infinity)*speed*Time.deltaTime;
+        Destroy(gameObject, activeTime);
+    }
+}
